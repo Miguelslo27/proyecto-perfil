891a31eb669832ec783a6b80da3de008fd52e7ae
Miguel <miguelmail2006@gmail.com>
2019-05-29 19:38:53 -0300

    feature/gulp-tasks - Added Teddy templating and refactor in tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,36 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var teddy = require('gulp-teddy').settings({
+  setTemplateRoot: 'src/templates/'
+});;
 
-gulp.task('build:dev', function() {
-  console.log("Build has started");
-  
+gulp.task('build:html', function () {
+  console.log('Build HTML has started');
+
   return gulp
-    .src('./src/*')
+    .src('./src/*.html')
+    .pipe(teddy.compile())
     .pipe(gulp.dest('./.temp'))
     .pipe(reload({ stream: true }));
 });
 
+gulp.task('build:files', function () {
+  console.log('Build files has started');
+
+  return gulp
+    .src([
+      './src/*',
+      '!./src/*.html'
+    ])
+    .pipe(gulp.dest('./.temp'))
+    .pipe(reload({ stream: true }));
+});
+
+gulp.task('build:dev', gulp.series(['build:html', 'build:files']));
+
 gulp.task('watch', gulp.series(['build:dev'], function(done) {
-  gulp.watch('./src/*', gulp.series(['build:dev']));
+  gulp.watch('./src/**/*', gulp.series(['build:dev']));
   done();
 }));
 
