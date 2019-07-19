08cb8fc78a42df57e2909c8f328b9ced16c50b45
Miguel <miguelmail2006@gmail.com>
2019-07-19 19:14:25 -0300

    feature/server-api-demo - Added demo api with cors

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ var path = require('path');
 var port = process.env.PORT || 8080;
 var env = process.env.ENV || 'DEV';
 var folderBuild = env == 'PROD' ? '/dist/' : '/.temp/';
+var cors = require('cors');
 
 server.use(express.static(path.join(__dirname, folderBuild)));
+server.use(cors());
 
 server.get('/', function (req, res) {
   // console.log('Esta es la raiz de mi sitio');
   // res.status(200).send('Bienvenido a mi servidor');
   return res.sendFile(path.join(__dirname, folderBuild, 'index.html'));
+});
+
+server.get('/api/datos', function (req, res) {
+  res.status(200).send({
+    name: "Miguel",
+    lastName: "Sosa"
+  });
 });
 
 server.get('/home', function (req, res) {
